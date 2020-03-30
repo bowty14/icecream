aeb1d7369ca4f603fc8a44586197610537b56556
Tyler Bowerman <tylerbowerman@yahoo.com>
2020-03-30 14:02:16 -0700

    add jquery functions to js file alerting you what flavors of icecream i like

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
- 
+  $(document).ready(function(){
+    $(".btn").click(function(event){
+    event.preventDefault();
+    var flavors = ['half-baked','double chocolate fudge', 'Oregon mudslide', 'cookie monster'];
+    flavors.forEach(function(flavor) {
+      alert('I love ' + flavor + " ice cream!!");
+    });
+  });
+});
